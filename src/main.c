050c69ea978f74bc05c8af29f7a3a2484cf791bf
6z7y <eenanho24n@proton.me>
2026-01-16 17:53:19 +0300

    rebuild main fn

--- src/main.c
+++ src/main.c
@@ -1,41 +1,69 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "control.h"
+// #include "control.h"
 #include "utils.h"
 
 #define PROG_NAME "tomu"
-#define PROG_VER "0.0.9"
+#define PROG_VER "0.0.10"
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2){
-    printf("Usage: %s [File.mp3]\n", PROG_NAME);
+  // 1. check user what want
+  if ( argc == 1 ){
+    printf("Usage: %s [FILE.mp3]\n", PROG_NAME); 
     return 0;
   }
 
-  const char *arg = argv[1];
-  const char *filename = argv[argc - 1];
+  char *option = argv[1];
+  char *path = argv[argc - 1];
 
-  if (argv[1][0] == '-' && argv[1][1] == '-') {
-    if (strcmp("--loop", arg) == 0)
-      path_handle(filename, true);
+  // 2. See what the user wants with "--" and handle it
+  if ( argv[1][0] == '-' && argv[1][1] == '-' ){
 
-    // // TODO fix a loop (Later)
-    // else if (strcmp("--shuffle-loop", arg) == 0)
-    //   shuffle(filename, true);
-    // // -----------------------------------------------
+    if ( strcmp("--loop", option ) == 0 ){
+      path_handle(path, true);
+      return 0;
+    }
 
-    else if (strcmp("--help", arg) == 0)
+    // TODO later
+    // else if ( strcmp("--shuffle", option) == 0 ){
+    //   if ( argc < 3 ){
+    //     printf("[T]: Please Provide what to shuffle\n");
+    //     return 1;
+    //   }
+    //
+    //   char *target = argv[2];
+    //
+    //   // shuffle with playlist
+    //   // meaning random with change in same path
+    //   // tomu --shuffle playlist [DIR]
+    //   if (strcmp("playlist", target) == 0)
+    //     shuffle(path, true);
+    //
+    //   else
+    //     printf("[T]: Unknown Shuffle target '%s'\n", target);
+    //
+    //   return 0;
+    // }
+
+    else if ( strcmp("--help", option) == 0 ){
       help();
+      return 0;
+    }
 
-    else if (strcmp("--version", arg) == 0)
-      printf("%s\n", PROG_VER);
+    else if ( strcmp("--version", option) == 0 ){
+      printf("Tomu: %s\n", PROG_VER);
+      return 0;
+    }
 
-    else 
-      printf("[T] Unknown Arg '%s'\n", arg);
+    else {
+      printf("[T]: unknown option '%s'\n", option);
+      return 0;
+    }
+  }
 
-  } else path_handle(filename, false);
-
+  // 3. No options? Just handle the path (check file or directory)  
+  path_handle(path, false);
   return 0;
 }
